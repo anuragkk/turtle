3ef782be9ff66eef2ff7e0546b517ab20b474da6
=Anurag <=anuragsnd@gmail.com>
2025-04-25 21:12:20 +0530

    turtle_dash

---
+++ main.py
@@ -0,0 +1,27 @@
+# from turtle import  Turtle, Screen
+# screen=Screen()
+#
+# tim= Turtle(shape="turtle")
+# tim.color("red")
+# for _ in range(50):
+#     tim.forward(2)
+#     tim.penup()
+#     tim.forward(2)
+#     tim.pendown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
