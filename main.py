103ed712c9192c6bdbfef234ca5ee59a863fd98d
=Anurag <=anuragsnd@gmail.com>
2025-04-25 23:24:52 +0530

    Added_random

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import random
 from turtle import Turtle, Screen
 
 screen = Screen()
 tim = Turtle(shape="turtle")
 tim.color("green")
-
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
+           "SeaGreen"]
+pensize =5
 for num_of_sides in range(3, 10):
+    tim.color(random.choice(colours))
+    tim.pensize(pensize)
 
     for _ in range(num_of_sides):
         angle = 360 / num_of_sides
         tim.forward(100)
         tim.right(angle)
+    pensize+=3
 
 screen.exitonclick()
