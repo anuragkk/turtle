280cc9f154a0b31f063756294d370cae344a06e1
=Anurag <=anuragsnd@gmail.com>
2025-04-25 22:32:37 +0530

    turtle_shapes

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
-# from turtle import  Turtle, Screen
-# screen=Screen()
-#
-# tim= Turtle(shape="turtle")
-# tim.color("red")
-# for _ in range(50):
-#     tim.forward(2)
-#     tim.penup()
-#     tim.forward(2)
-#     tim.pendown()
+from turtle import Turtle, Screen
 
+screen = Screen()
+tim = Turtle(shape="turtle")
+tim.color("green")
 
+for num_of_sides in range(3, 10):
 
-
-
-
-
-
-
-
-
-
-
-
-
+    for _ in range(num_of_sides):
+        angle = 360 / num_of_sides
+        tim.forward(100)
+        tim.right(angle)
 
 screen.exitonclick()
