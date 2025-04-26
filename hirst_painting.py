11554828f4917e57dea35e15f4c3fb568fcad0b5
=Anurag <=anuragsnd@gmail.com>
2025-04-26 15:21:09 +0530

    corrected

---
+++ hirst_painting.py
@@ -0,0 +1,7 @@
+from turtle import Turtle, Screen
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
+           "SeaGreen"]
+screen = Screen()
+tim = Turtle()
+tim.
+tim.circle(2)
